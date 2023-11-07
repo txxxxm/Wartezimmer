08737b654a3901c439d3090c231c87c004061758
tim.asmus <timasmus1@gmail.com>
2023-11-07 13:25:32 +0100

    Initial commit

---
+++ src/Patient.java
@@ -0,0 +1,13 @@
+public class Patient {
+    String name;
+    String nachname;
+    Patient nachfolger;
+
+    public void Patient(String pname, String pnachname) {
+        name = name;
+        nachname = nachname;
+        nachfolger = nachfolger;
+        
+
+    }
+}
